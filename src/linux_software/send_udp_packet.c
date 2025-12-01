e4bc3c1b8e8bebbc0caf9706b80249be1bd8d0ba
ccaugelli <ccaugelli@DESKTOP-NSC8HIO.localdomain>
2025-11-30 19:42:41 -0500

    Integrated scripts to stream FIFO data over UDP.

--- src/linux_software/send_udp_packet.c
+++ src/linux_software/send_udp_packet.c
@@ -13,15 +13,11 @@
 // Example: 
 //  root@zybo_linux:/run/media/SOC-mmcblk0p1# ./send_udp_packet 192.168.100.74 5005 10
 
-int main(int argc, char *argv[]) {
+int send_udp_packet(const char *UDP_IP, int UDP_PORT, const char *MESSAGE) {
     if (argc < 4) {
-        fprintf(stderr, "Usage: %s <UDP_IP> <UDP_PORT> <num_packets>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <UDP_IP> <UDP_PORT> <MESSAGE>\n", argv[0]);
         return 1;
     }
-
-    const char *UDP_IP = argv[1];
-    int UDP_PORT = atoi(argv[2]);
-    int num_packets = atoi(argv[3]);
 
     // Create UDP socket
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -42,7 +38,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Message to send
-    const char *MESSAGE = "Hello, UDP!";
+    // const char *MESSAGE = "Hello, UDP!";
 
     // Send the message
     ssize_t sent_bytes = sendto(sock, MESSAGE, strlen(MESSAGE), 0,
@@ -53,29 +49,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     printf("UDP packet sent to %s:%d with message: %s\n", UDP_IP, UDP_PORT, MESSAGE);
-
-    int counter = 0;
-    while (counter < num_packets) {
-        counter++;
-        char buffer[256];
-        int len = snprintf(buffer, sizeof(buffer), "counter value: %d", counter);
-        if (len < 0 || len >= (int)sizeof(buffer)) {
-            fprintf(stderr, "Message formatting error\n");
-            close(sock);
-            return 1;
-        }
-
-        sent_bytes = sendto(sock, buffer, len, 0,
-                            (const struct sockaddr *)&servaddr, sizeof(servaddr));
-        if (sent_bytes < 0) {
-            perror("sendto failed");
-            close(sock);
-            return 1;
-        }
-        printf("UDP packet sent to %s:%d with message: %s; counter = %d\n", UDP_IP, UDP_PORT, buffer, counter);
-    }
-
-    printf("Exiting loop after sending %d packets.\n", num_packets);
 
     close(sock);
     return 0;
